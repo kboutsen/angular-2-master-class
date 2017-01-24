2e4cfa58a2b70ad34c0a19d778ce93be45941d11
Boutsen Koen <koen.boutsen@cegeka.com>
2017-01-24 13:53:23 +0100

    Redirect /

--- src/app/contacts-detail-view/contacts-detail-view.component.ts
+++ src/app/contacts-detail-view/contacts-detail-view.component.ts
@@ -24,7 +24,7 @@ export class ContactsDetailViewComponent implements OnInit {
   }
 
   navigateToList () {
-    this.router.navigate(['../'], {
+    this.router.navigate(['/'], {
       relativeTo: this.route
     });
   }
